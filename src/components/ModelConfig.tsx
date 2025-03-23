5e5370056697c0776ddc2d79176cabe790b20b72
Jorben <jorbenzhu@gmail.com>
2025-03-23 18:51:15 +0800

    style: auto height for ModelConfig

--- src/components/ModelConfig.tsx
+++ src/components/ModelConfig.tsx
@@ -6,7 +6,7 @@ const ModelConfig: React.FC = () => {
   return (
     <div>
       <Tabs
-        style={{ minHeight: '645px' }}
+        style={{ height: 'calc(100vh - 180px)' }}
         defaultActiveKey="0"
         tabPosition="left"
         items={Array.from({ length: 5 }, (_, i) => {
